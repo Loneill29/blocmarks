84d4caad0aa38737c763236e5ffaea648585aa59
Linda <oneill.linda21@gmail.com>
2018-05-18 14:32:05 -0500

    apply revisions

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
 
+  get 'bookmarks/show'
+
+  get 'bookmarks/new'
+
+  get 'bookmarks/edit'
+
   devise_for :users
 
   get 'welcome/index'
 
   get 'welcome/about'
 
-  resources :topics
+  resources :topics do
+    resources :bookmarks
+  end
 
   root 'welcome#index'
 end
