6e7464d74861baa8ba9526a8a171adbfa3b7e0e9
Linda <oneill.linda21@gmail.com>
2018-05-19 11:14:17 -0500

    fix heroku error

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,12 @@
 
 ActiveRecord::Schema.define(version: 20180515170711) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bookmarks", force: :cascade do |t|
     t.string "url"
-    t.integer "topic_id"
+    t.bigint "topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["topic_id"], name: "index_bookmarks_on_topic_id"
@@ -22,7 +25,7 @@ ActiveRecord::Schema.define(version: 20180515170711) do
 
   create_table "topics", force: :cascade do |t|
     t.string "title"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_topics_on_user_id"
@@ -50,4 +53,6 @@ ActiveRecord::Schema.define(version: 20180515170711) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookmarks", "topics"
+  add_foreign_key "topics", "users"
 end
